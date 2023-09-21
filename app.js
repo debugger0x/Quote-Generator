77f16896816beb10f1bd64d0d43ab1b6fab2530e
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-09-21 14:06:40 +0100

    Oh yeahhhh, ready to be deployed

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+// Buttons
+const newQuoteBtn = document.getElementById('new-quote');
+const author = document.getElementById('author');
+const quoteText = document.getElementById('quote');
+// Functions
 function newQuote() {
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   console.log(quote);
+  author.textContent = quote.author;
+
+  if (!quote.text) {
+    quoteText.textContent = quote.quote;
+  } else {
+    quoteText.textContent = quote.text;
+  }
+  // Replacing its text content
 }
 
-newQuote();
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
